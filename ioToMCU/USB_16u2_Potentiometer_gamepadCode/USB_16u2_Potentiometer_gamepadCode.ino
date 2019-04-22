7cb4c4f3729cb2138f9e39f7dd6daa6191a00051
S. Skaugvoll <skaugvoll@gmail.com>
2019-04-23 01:03:34 +0200

    added todo, and removed a few println

--- ioToMCU/USB_16u2_Potentiometer_gamepadCode/USB_16u2_Potentiometer_gamepadCode.ino
+++ ioToMCU/USB_16u2_Potentiometer_gamepadCode/USB_16u2_Potentiometer_gamepadCode.ino
@@ -1,9 +1,22 @@
+/*
+
+TODO, on the IO chip,
+map the values from pot. from 0 - 1023 to 0 - 255,
+then send the value over the Serial.
+
+On the USB - MCU chip, map the values from 0 - 255 to 0 - 9,
+since it makes big interval into a small, requires bigger change on the value to "skip to next value on small interval".
+
+Reason for / Idea of this changes is that the potentiometer sends values bigger then 255m but they are overflown and restarts from 0, over the serial
+*/
+
+
 #include "HID-Project.h"
 //#include "HID.h"
 
 
 // DECLARE CONSTANTS
-const int reading_pause = 10;
+const int reading_pause = 100;
 const int ignition = 1; // WHERE TO READ THE IGNITION FROM HARDWARE
 const int starter = 2; // WHERE TO READ THE STARTER FROM HARDWARE
 
@@ -16,6 +29,12 @@ uint8_t starterValue = 0;
 uint8_t starterControllerButton = 3;
 
 uint16_t prevHandbrake = 0;
+
+
+int treatValue(int data) {
+    return (data * 9 / 256);
+  }
+
 
 void setup() {
   // Start the Serial1 which is connected with the IO MCU.
@@ -35,39 +54,46 @@ void setup() {
 
   // Sends a clean report to the host. This is important on any Arduino type.
   Gamepad.begin();
-
 }
 
 void loop() {
+  // Relsease (unpress all the buttons);
+//  Gamepad.release(ignitionControllerButton);
+//  Gamepad.release(starterControllerButton);
 
   // ############   Potentiometer / handbrake
   // Check if any Serial data from the IO MCU was received (-1 if no data)
   uint16_t handbrake = Serial1.read();
   
-
-  Serial.print(F("USB: "));
+  // If it's a character, print it!
+//  if (c >= 'a' && c <= 'z') {
+//    Serial.print(F("USB: "));
+//    Serial.print(c);
+//    Keyboard.print(c);
+//  }
+  //Serial.print(F("USB: "));
   Serial.print(handbrake);
   Serial.print(" :: ");
-  Serial.println(map(handbrake, 0, 255, 0, 0xFFFF));
-
-  //  To make this work. plug the handbrake into the computer. open controll board, select devices and printers. right click on hoodloader, properties, then settings, then calibrate. when the z-axis is next to callibrate pull the handbrake halfway, then start the calibration
-  // pull to full and then release (no pull) then turn on the igniton to actuall save the calibration, (pressing next does not work), then, press next all the way, and finally finished to save.
-  // Now unplugg the handbrake and replug it into the computer. now it should work. there is a bit of lag, but I'm working on it.
-  handbrake = map(handbrake, 0, 255, 0, 50);
-
+  //Serial.println(map(handbrake, 0, 255, 0, 0xFFFF));
+  //Serial.println();
+  
+  // Map the potentiometer value from 0-255 to the full range of 16bit, to make the movement in direction noticable
+  //handbrake = map(handbrake, 0, 255, 0, 60);
+  handbrake = treatValue(handbrake);
+  Serial.println(handbrake);
   // Move the Y axis on the "controller"
-  if (prevHandbrake != handbrake){
-    Gamepad.zAxis(handbrake);
-    prevHandbrake = handbrake;
-  }
+  //if (prevHandbrake != handbrake){
+  Gamepad.zAxis(handbrake);
+    //prevHandbrake = handbrake;
+  //}
 
 
 // Tenningslaas
   // ############   ignition
   ignitionValue = digitalRead(ignition);
-  Serial.print("IGNITION: ");
-  Serial.println(ignitionValue);
-  delay(100);
+  //Serial.print("IGNITION: ");
+  //Serial.println(ignitionValue);
+  delay(50);
   
   // Press the ignition button on  a gamecontroller
   if (ignitionValue == 1) {
@@ -78,8 +104,8 @@ void loop() {
   }
   // ############   starter
   starterValue = digitalRead(starter);
-  Serial.print("Starter: ");
-  Serial.println(starterValue);
+  //Serial.print("Starter: ");
+  //Serial.println(starterValue);
 
   // Press the starterbutton button on a gamecontroller
   if (starterValue == 1){
@@ -95,8 +121,6 @@ void loop() {
   
   // Delay reading because the IO chip has to have a pause to get the correct potentionmeter value 
   delay(reading_pause);
-
-  
 
   
 }
